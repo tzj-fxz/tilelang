0fa4150726dd7b1229ee90aee5a78a45ed02e88e
Tong WU <109033598+Rachmanino@users.noreply.github.com>
2026-01-09 23:37:05 +0800

    [Example] Add attention sink varlen examples (#1645)
    * add gqa sink varlen fwd
    
    * upd bwd
    
    * add test
    
    * fix causal bwd
    
    * lint

--- examples/attention_sink/test_example_attention_sink.py
+++ examples/attention_sink/test_example_attention_sink.py
@@ -5,6 +5,8 @@ import example_mha_sink_fwd_bhsd_wgmma_pipelined
 import example_gqa_sink_fwd_bhsd_wgmma_pipelined
 import example_mha_sink_bwd_bhsd
 import example_gqa_sink_bwd_bhsd
+import example_gqa_sink_fwd_varlen
+import example_gqa_sink_bwd_varlen
 
 
 @tilelang.testing.requires_cuda
@@ -59,6 +61,13 @@ def test_example_gqa_sink_bwd_bhsd():
 @tilelang.testing.requires_cuda
 def test_example_gqa_sink_bwd_bhsd_sliding_window():
     example_gqa_sink_bwd_bhsd.main(window_size=128)
+
+
+@tilelang.testing.requires_cuda
+@tilelang.testing.requires_cuda_compute_version_ge(9, 0)
+def test_example_gqa_sink_varlen():
+    example_gqa_sink_fwd_varlen.main()  # non-causal
+    example_gqa_sink_bwd_varlen.main()  # causal
 
 
 if __name__ == "__main__":
