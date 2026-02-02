90daf3e12b2756a8c52c64e59bd8eda0c40b4535
tzj-fxz <tzjfxz@gmail.com>
2026-02-02 23:11:12 +0800

    Lint

--- testing/python/issue/test_tilelang_issue_1729.py
+++ testing/python/issue/test_tilelang_issue_1729.py
@@ -7,21 +7,13 @@ def test_issue_1729():
     @tilelang.jit
     def get_qwq():
         @T.prim_func
-        def main(
-            A: T.Tensor[(2, 2560), T.float32],
-            B: T.Tensor[(2, 2560), T.float32],
-            C: T.Tensor[(2, ), T.float32]
-        ):
+        def main(A: T.Tensor[(2, 2560), T.float32], B: T.Tensor[(2, 2560), T.float32], C: T.Tensor[(2,), T.float32]):
             with T.Kernel(1, threads=256):
-                tx = T.get_thread_binding(0)
-
                 A_local = T.alloc_fragment((2, 2560), T.float32)
                 B_local = T.alloc_fragment((2, 2560), T.float32)
-                C_local = T.alloc_fragment((2, ), T.float32)
+                C_local = T.alloc_fragment((2,), T.float32)
 
-                T.annotate_layout({
-                    C_local: tilelang.layout.make_fully_replicated_layout_fragment(C_local, 256)
-                })
+                T.annotate_layout({C_local: tilelang.layout.make_fully_replicated_layout_fragment(C_local, 256)})
 
                 T.copy(A, A_local)
                 T.copy(B, B_local)
